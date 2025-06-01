496a14400988a111a18dc804b3439bb3beff0eab
com8stem <145276553+com8stem@users.noreply.github.com>
2025-06-01 15:05:45 +0900

    init.sql: update user creation to use environment variables; script.sh: fix user creation command and ensure proper variable usage

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -8,13 +8,12 @@ curl -o wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost=mariadb --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser="$WORDPRESS_DB_USER2" --dbpass="$WORDPRESS_DB_PASSWORD2" --dbhost=mariadb --allow-root
 
 ./wp-cli.phar config set DISALLOW_FILE_EDIT false --allow-root
 ./wp-cli.phar config set DISALLOW_FILE_MODS false --allow-root
 ./wp-cli.phar config set FS_METHOD direct --allow-root
 
-./wp-cli.phar core install --url=DOMAIN_NAME --title=inception --admin_user=WORDPRESS_ADMIN_USER --admin_password=WORDPRESS_ADMIN_PASSWORD --admin_email=admin@admin.com --allow-root --skip-email
+./wp-cli.phar core install --url="$DOMAIN_NAME" --title=inception --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email=admin@admin.com --allow-root --skip-email
 
 chown -R www-data:www-data /var/www/html
 exec php-fpm8.2 -F
