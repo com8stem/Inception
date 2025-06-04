ab79efbf6a8c0745e4876832962b7c1759f93bbc
com8stem <145276553+com8stem@users.noreply.github.com>
2025-06-04 14:33:17 +0900

    fix: update MariaDB configuration and initialization scripts

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,6 +4,11 @@ cd /var/www/html
 
 rm -rf ./*
 
+echo "waiting for mariadb to start"
+while ! mysqladmin ping -h "mariadb" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" --silent; do
+    sleep 1
+done
+
 curl -o wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
@@ -13,7 +18,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config set DISALLOW_FILE_MODS false --allow-root
 ./wp-cli.phar config set FS_METHOD direct --allow-root
 
-./wp-cli.phar core install --url="$DOMAIN_NAME" --title=inception --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email=admin@admin.com --allow-root --skip-email
+./wp-cli.phar core install --url="$DOMAIN_NAME" --title=inception --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --allow-root --skip-email
 
 chown -R www-data:www-data /var/www/html
 exec php-fpm8.2 -F
